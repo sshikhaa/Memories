f602151e36ad7bebf0a7b5c6ee0032352e38c32b
sshikhaa <108489237+sshikhaa@users.noreply.github.com>
2022-08-19 23:53:56 +0530

    Delete client directory

--- client/src/constants/actionTypes.js
+++
@@ -1,9 +0,0 @@
-export const CREATE = 'CREATE';
-export const UPDATE = 'UPDATE';
-export const DELETE = 'DELETE';
-export const FETCH_ALL = 'FETCH_ALL';
-export const LIKE = 'LIKE'
-//this makes us to find error easily if it exists in spelling there vars and making code less error-prone
-
-export const AUTH = 'AUTH';
-export const LOGOUT = 'LOGOUT';
